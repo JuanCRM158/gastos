6f93d8df71e34cecbea3478a9527e7f475ac1bf5
Juan Carlos Ramirez Martinez <72681620+JuanCRM158@users.noreply.github.com>
2025-11-02 16:23:43 -0700

    Add files via upload

---
+++ chunk-3LUN3LFJ.js
@@ -0,0 +1,1 @@
+import{i as a,j as b,k as c,l as d,m as e,o as f}from"./chunk-PLI7252T.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
---
+++ chunk-QRMDBHER.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-NVARE33S.js";import"./chunk-PLI7252T.js";export{a as supabase};
